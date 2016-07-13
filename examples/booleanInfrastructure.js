19efb531f246e6ec4c836f2df7584359015a265a
Jesse Mu <muj@bc.edu>
2016-07-12 17:26:23 -0700

    Compute DAG JPDs

--- examples/booleanInfrastructure.js
+++ examples/booleanInfrastructure.js
@@ -4,12 +4,12 @@ var infrastructure = function() {
     // TODO: This also assumes discrete judgments (for now), that come from
     // probs
     var discrete = true;
-    var probs = [0, 0.5];
+    // var probs = [0, 0.5];
     var weights = [-1, 1];
 
-    // var probs = [
-        // 0, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99, 1
-    // ];
+    var probs = [
+        0, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99, 1
+    ];
     // var weights = [
         // -0.01, -0.05, -0.1, -0.2, -0.3, -0.4, -0.5, -0.6, -0.7, -0.8, -0.9,
         // -0.95, -0.99, -1, 0, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7,
@@ -23,6 +23,14 @@ var infrastructure = function() {
         var i = randomInteger(toShuffle.length);
         var toRemove = toShuffle[i];
         return shuffle(_.without(toShuffle, toRemove), append(shuffled, toRemove));
+    };
+
+    var reflit = function(arr, f) {
+        return filter(f, arr);
+    };
+
+    var lla = function(arr, pred) {
+        return all(pred, all);
     };
 
     var pam = function(arr, f) {
@@ -50,90 +58,121 @@ var infrastructure = function() {
         console.log(error);
     };
 
-    // Calculate P(A).
-    // Because we're restricting ourselves to n = 3, exact inference is
-    // possible.
-    // XXX: cache might overflow, we're caching a LOT of things
-    // XXX: What happens when w0 = 1, w1 = -1? Output is 1, is that correct?
-    var marginal = dp.cache(function(aList, aWeights, aPriors, a) {
-        // Lemmer & Gossnik RNOR. Works associatively in the cases of noisy ORs.
-        // FIXME: Consider dependent clauses (see RNOR paper)
-        // FIXME: This doesn't work associatively for noisy ANDS! Use
-        // something like Yuille (2008)
-        // Also works for the cases where parents are empty and parents have
-        // length 1
-        var initw0 = aPriors[a];
-        var parentsAndWeights = zip(aList[a], aWeights[a]);
-        return ecuder(parentsAndWeights, initw0, function(pandw, w0) {
-            var parent = pandw[0];
-            var w1 = pandw[1];
-
-            var parentProb = marginal(aList, aWeights, aPriors, parent);
-
-            // Component 1: a happens in the absence of parentProb (c = 0)
-            var cmp1 = w0;
-            // Component 2: probabiltiy of w manifesting in presence of parentProb,
-            // either generative or preventive
-            var cmp2 = (w1 >= 0) ?
-                1 - ((1 - w0) * (1 - w1)) : // Noisy-or
-                w0 * (1 + w1); // Noisy-nand
-
-            return cmp1 * (1 - parentProb) + cmp2 * parentProb;
+    // Calculate the JPD table
+    var JPD = function(aList, aWeights, aPriors) {
+        // Identify global parent nodes
+        var parents = reflit(Object.keys(aList), function(node) {
+            return aList[node].length === 0;
         });
-    });
-
-    // Calculate P(A | B). Same XXXs as above.
-    // TODO: This needs to work forwards and backwards
-    // Need a more general formula for this.
-    // Although inefficient, in the n = 3 case, this is reasonable: just
-    // calculate using joint probability and marginal formulas:
-    // P(C | B) = P(B, C) / P(B)
-    // P(B | C) = P(B, C) / P(C)
-    // ^ Marginal, and P(B, C) = \sum_{A}P(A, B, C) = \prod P(X_i | Parents(X_i))
-    // In the backwards (explanation) direction:
-    // P(B | A):
-    // check if A's ancestry contains B.
-    // If yes, then since this is a DAG, you need to work backwards.
-    // Otherwise, you need to work forwards. It could be that indeed B's
-    // ancestry doesn't contain A either, but the propagation will simply not
-    // condition on the evidence, which is fine.
-    var conditional = dp.cache(function(aList, aWeights, aPriors, a, b, bTruth) {
-        var initw0 = aPriors[a];
-        var parentsAndWeights = zip(aList[a], aWeights[a]);
-        return ecuder(parentsAndWeights, initw0, function(pandw, w0) {
-            var parent = pandw[0];
-            var w1 = pandw[1];
-
-            var parentProb =
-                // Current parent is B, and we're conditioning on B = true.
-                // probability of the parent occurring <- 1.
-                (pandw[0] === b && bTruth) ? 1 :
-                // Current parent is B and condition on B = false.
-                // Probability of the parent occurring <- 0
-                (pandw[1] === b && !bTruth) ? 0 :
-                // Otherwise calculate conditional recursively, propagating the
-                // condition of b/bTruth
-                conditional(aList, aWeights, aPriors, pandw[0], b, bTruth);
-
-            // OPTIMIZE: calculating both components even if parentProb is 1 or 0.
-            var cmp1 = w0;
-            var cmp2 = (w1 >= 0) ?
-                1 - ((1 - w0) * (1 - w1)) :
-                w0 * (1 + w1);
-
-            return cmp1 * (1 - parentProb) + cmp2 * parentProb;
+        var parProbs = pam(parents, function(p) {
+            return aPriors[p];
         });
-    });
+
+        var children = reflit(Object.keys(aList), function(node) {
+            return aList[node].length !== 0;
+        });
+
+        return Enumerate(function() {
+            // Parents occur with probability equal to background cause
+            var parAssns = pamN(parents.length, function(i) {
+                return flip(parProbs[i]);
+            });
+            // FIXME: after probmods/webppl#552
+            // var parAssns = sample(MultivariateBernoulli({
+                // ps: parProbs
+            // }));
+            var parObj = _.object(parents, parAssns);
+            // Loop through children. If some of the parents aren't assigned,
+            // skip that node. We want information to proceed in a "logical"
+            // manner
+            var assign = function(chiObj) {
+                var newChiObj = pamObject(chiObj, function(node, val) {
+                    if (val == null) {
+                        // Obtain parent values
+                        var childPars = aList[node];
+                        var parAssns = pam(childPars, function(c) {
+                            // Either assigned as other children, or assigned
+                            // as parents
+                            // Need to check for presence of val, but can't use
+                            // truthiness, since 0 is false
+                            var p1 = chiObj[c];
+                            var p2 = parObj[c];
+                            return (p1 != null) ? p1 : p2;
+                        });
+                        var allParentsAssigned = all(function(c) {
+                            return c != null;
+                        }, parAssns);
+                        if (allParentsAssigned) {
+                            // Yuille (2008) noisy-logical.
+                            // XXX: For the case of one generative cause ~G, 
+                            // one preventive ~P, and one background cause ~B,
+                            // we assume the boolean function (~G ^ ~P) v ~B.
+                            // This is a rather arbitrary selection, and WON'T
+                            // work if expanding to 4 boolean predicates.
+                            // Alternatively, you'd need to sample from the
+                            // boolean functions.
+
+                            // The probability of c occurring absent of causes
+                            var initw0 = aPriors[node];
+
+                            // Assignments to the parents, and their weights
+                            var parentsAndWeights = zip(parAssns, aWeights[node]);
+
+                            var prob = ecuder(parentsAndWeights, initw0, function(p, w0) {
+                                var parent = p[0];
+                                var w1 = p[1];
+
+                                // If parent is not ON, then whatever effect
+                                // (generative/preventive) won't manifest, so
+                                // leave w0 alone
+                                if (!parent) {
+                                    return w0;
+                                } else {
+                                    // Otherwise, calculate increased/decreased probability
+                                    var cmp2 = (w1 >= 0) ?
+                                        1 - ((1 - w0) * (1 - w1)) : // Noisy-or
+                                        w0 * (1 + w1); // Noisy-not-and
+                                    return cmp2;
+                                }
+                            });
+
+                            // Flip according to this probability
+                            return flip(prob);
+                        } else {
+                            // Wait for other children to be assigned
+                            return null;
+                        }
+                    } else {
+                        // Value has already been sampled; return value as is
+                        return val;
+                    }
+                });
+
+                if (_.values(newChiObj).indexOf(null) !== -1) {
+                    // Some still unassigned
+                    return assign(newChiObj);
+                } else {
+                    return newChiObj;
+                }
+            };
+
+            var nullAssns = pamN(children.length, function(c) { return null; });
+            var chiObj = assign(_.object(children, nullAssns));
+            // Return these in aList order
+            return pam(Object.keys(aList), function(node) {
+                return (parObj[node] != null) ? parObj[node] : chiObj[node];
+            });
+        });
+    };
 
     // a DAG functor, taking in an adjacency list (structure), a set of weights
     // (strength), and returning a function that scores experiments according to responses.
     var DAG = function(aList, aWeights, aPriors) {
-        // aList is an object connecting vertices
-        // aList is a set of weights for each connection.
+        // XXX: Store JPD here, or add to a cache?
+        var jpd = JPD(aList, aWeights, aPriors);
+        // Numerical IDs for ordering of JPDS
+        var ids = _.object(Object.keys(aList), _.range(Object.keys(aList).length));
         var dagModel = function(x, y) {
-            // TODO: Is this the right way of encoding human noisiness (by
-            // making the parameter estimates of these models themselves
-            // noisy?)
             if (x.type === 'structure') {
                 // Any way to make this faster than these linear checks...hash?
                 var pIndex = aList[x.child].indexOf(x.parent);
@@ -145,28 +184,25 @@ var infrastructure = function() {
                     // Preventive effect - - negative weight in parents
                     (y === -1) && (pIndex !== -1) && aWeights[x.child][pIndex] < 0
                 );
-                // To make this delta, p should be 1
-                // Let's say 0.9, instead
+                // To make this delta, p should be 1. For noise, decrease p
                 return Bernoulli({p: 0.9}).score(compatible);
             } else if (x.type === 'marginal') {
-                var marginalMAP = marginal(aList, aWeights, aPriors, x.a);
-                return Gaussian({
-                    mu: marginalMAP,
-                    sigma: 0.1
+                var marginalEst = marginal(jpd, ids[x.a]);
+                return Binomial({
+                    n: 100,
+                    p: marginalEst
                 }).score(y);
             } else if (x.type === 'conditional') {
-                var conditionalMAP = conditional(
-                    aList, aWeights, aPriors,
-                    x.a, x.b, x.bTruth
-                );
-                return Gaussian({
-                    mu: conditionalMAP,
-                    sigma: 0.1
+                var conditionalEst = conditional(jpd, ids[x.a], x.rest);
+                return Binomial({
+                    n: 100,
+                    p: conditionalEst
                 }).score(y);
             } else {
                 err("unknown type " + x.type);
             }
         };
+        // Don't encode JPDs into model name, too verbose
         var mName = JSON.stringify([aList, aWeights, aPriors]);
         return Model(mName, dagModel);
     };
@@ -184,16 +220,11 @@ var infrastructure = function() {
         // higher rank edges.
         var edges = pamN(orderedNodes.length, function(i) {
             var toNodes = _.rest(orderedNodes, i + 1);
-            // Keep edges with probability equal
+            // Keep edges with probability equal (normalize later)
             return sort(retlif(toNodes, function() {
                 flip(0.5);
             }));
         });
-
-
-        // Sample from noisy-or vs noisy-nand
-        // inhibit
-        // TODO: when identified joint causes, sample from noisy-or vs noisy-nand
 
         var unsorted = _.object(orderedNodes, edges);
 
@@ -211,24 +242,22 @@ var infrastructure = function() {
     });
 
     var sampleWeights = function(aList) {
-        // Create another adjacency-list-esque thing, but instead containing
-        // weights
+        // Sample causal weights
         var aWeights = pamObject(aList, function(from, tos) {
             return pam(tos, function(t) {
                 if (discrete) {
                     return uniformDraw(weights);
                 } else {
-                    // Uniform prior on weights?...but they should be negative?
                     // Alternatively, scale a Beta distribution
                     return sample(Uniform({a: -1, b: 1}));
                 }
             });
         });
-        // TODO: What are the data structures for the weights
         return aWeights;
     };
 
     var samplePriors = function(aList) {
+        // Sample background weights ("priors?")
         var aPriors = pamObject(aList, function(from, tos) {
             // Ignore tos, just sample a probability
             if (discrete) {
@@ -252,18 +281,52 @@ var infrastructure = function() {
     };
 
     // Sample a DAG
+    // var mSample = function() {
+        // var structAdjList = uniformDraw(enumerateStructures(nodes));
+        // var structAdjWeights = sampleWeights(structAdjList);
+        // var structAdjPriors = samplePriors(structAdjList);
+        // return DAG(structAdjList, structAdjWeights, structAdjPriors);
+    // };
     var mSample = function() {
-        var structAdjList = uniformDraw(enumerateStructures(nodes));
-        var structAdjWeights = sampleWeights(structAdjList);
-        var structAdjPriors = samplePriors(structAdjList);
-        return DAG(structAdjList, structAdjWeights, structAdjPriors);
+        return uniformDraw([
+            // Null
+            DAG(
+                {bright: [], hot: [], on: []},
+                {bright: [], hot: [], on: []},
+                {bright: 0.5, hot: 0.5, on: 0.5}
+            ),
+            // half bulbs are on, and all are bright + hot
+            DAG(
+                {bright: ['on'], hot: ['on'], on: []},
+                {bright: [1], hot: [1], on: []},
+                {bright: 0, hot: 0, on: 0.5}
+            ),
+            // Hot causes brightness
+            DAG(
+                {bright: ['hot'], hot: ['on'], on: []},
+                {bright: [1], hot: [1], on: []},
+                {bright: 0, hot: 0, on: 0.5}
+            ),
+            // "Some bulbs are eco-friendly"
+            // DAG(
+                // {bright: ['on'], hot: ['on'], on: []},
+                // {bright: [1], hot: [0.8], on: []},
+                // {bright: 0, hot: 0, on: 0.5}
+            // ),
+            // "Most lightbulbs are off"
+            // DAG(
+                // {bright: ['on'], hot: ['on'], on: []},
+                // {bright: [1], hot: [1], on: []},
+                // {bright: 0, hot: 0, on: 0.2}
+            // )
+        ]);
     };
 
     // Sample an experiment
+    // TODO: For now, omit structure
     var xSample = function() {
-        // Then later, conditional
-        var xType = uniformDraw(['structure', 'marginal', 'conditional']);
-        // var xType = 'structure';
+        // var xType = uniformDraw(['structure', 'marginal', 'conditional']);
+        var xType = uniformDraw(['marginal', 'conditional']);
         if (xType === 'structure') {
             var pair = randomPair(nodes);
             var child = pair[0],
@@ -285,24 +348,27 @@ var infrastructure = function() {
                 type: xType,
                 a: a,
                 text: (
-                    "Image 100 x. How many are " + a +
+                    "Imagine 100 x. How many are " + a +
                     "? in [0, 1]"
                 )
             };
         } else if (xType === 'conditional') {
-            var pair = randomPair(nodes);
-            // TODO: Is there a more proper notation for this than just
-            // assuming P(A|B)?
-            var a = pair[0],
-                b = pair[1];
+            var a = nodes[randomInteger(nodes.length)];
+            var rest = _.without(nodes, a);
+            // How many other variables to condition on
+            var condAmt = randomInteger(rest.length) + 1;
+            // Assign random varible to each rest
+            var condNodes = sampleN(rest, condAmt);
+            var cond = _.object(
+                condNodes,
+                repeat(condAmt, flip)
+            );
             return {
                 type: xType,
                 a: a,
-                b: b,
-                // Either P(A | B) or P(A | ~B)
-                bTruth: flip(),
+                cond: cond,
                 text: (
-                    "Imagine 100 x that are " + b +
+                    "Imagine 100 x that are " + cond +
                     ". What proportion are " + a +
                     "? in [0, 1]"
                 )
@@ -313,14 +379,8 @@ var infrastructure = function() {
     var ySample = function(x) {
         if (x.type === 'structure') {
             return uniformDraw([-1, 0, 1]);
-        } else if (x.type === 'marginal') {
-            // Totally uninformative prior
-            if (discrete) {
-                return uniformDraw(probs);
-            } else {
-                return sample(Beta({a: 1, b: 1}));
-            }
-        } else if (x.type === 'conditional') {
+            // Responses on marginal/conditionals are continuous
+        } else if (x.type === 'marginal' || x.type === 'conditional') {
             // Totally uninformative prior
             if (discrete) {
                 return uniformDraw(probs);
@@ -357,22 +417,22 @@ var infrastructure = function() {
             },
             X: Enumerate,
             // Only 6, but if using predictive y, then need approximation
-            Y: function(thunk) {
-                Infer({
-                    method: 'enumerate',
-                    maxExecutions: 100,
-                    // See notes on likelyFirst favoring MAP
-                    strategy: 'likelyFirst'
-                }, thunk);
-            }
-            // Y: !discrete && function(thunk) {
-                // console.log('mcmc');
+            // Y: function(thunk) {
                 // Infer({
-                    // method: 'MCMC',
-                    // kernel: 'MH',
-                    // samples: 10,
+                    // method: 'enumerate',
+                    // maxExecutions: 100,
+                    // // See notes on likelyFirst favoring MAP
+                    // strategy: 'likelyFirst'
                 // }, thunk);
             // }
+            Y: !discrete && function(thunk) {
+                console.log('mcmc');
+                Infer({
+                    method: 'MCMC',
+                    kernel: 'MH',
+                    samples: 10,
+                }, thunk);
+            }
         }
     };
 };

01d18458c3ec9f6f124a2be3252585ca9ce55a64
Jesse Mu <muj@bc.edu>
2016-07-20 16:24:42 -0700

    Include var declaration in caching

--- examples/cacheStructure.js
+++ examples/cacheStructure.js
@@ -5,9 +5,12 @@ var fs = require('fs');
 var aoed = adaptive.AOED(infrastructure);
 
 var fname = "./cache/structure.json";
+var varname = "structureCache";
+
+var content = 'var ' + varname + ' = ' + JSON.stringify(cache) + ';';
 
 var cache = aoed.cache();
-fs.writeFile(fname, JSON.stringify(cache), function(err) {
+fs.writeFile(fname, content, function(err) {
     if (err) return console.log(err);
 
     console.log("Saved", fname);

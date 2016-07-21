1baf41504f677ff92ba120f187ce1de9a75dc58a
Jesse Mu <muj@bc.edu>
2016-07-21 15:45:42 -0700

    bugfix: reorder cache statement

--- examples/cacheStructure.js
+++ examples/cacheStructure.js
@@ -7,9 +7,9 @@ var aoed = adaptive.AOED(infrastructure);
 var fname = "./cache/structure.json";
 var varname = "structureCache";
 
+var cache = aoed.cache();
 var content = 'var ' + varname + ' = ' + JSON.stringify(cache) + ';';
 
-var cache = aoed.cache();
 fs.writeFile(fname, content, function(err) {
     if (err) return console.log(err);
 
